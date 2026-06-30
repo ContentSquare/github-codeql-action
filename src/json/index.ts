9939a54a819182b6d5d0027986025a71fc3a89ca
Mario Campos <mario-campos@github.com>
2026-06-30 16:39:00 -0500

    Change `optional` to include `undefined`
    The previous implementation of `optional` is now `optionalOrNull`.

--- src/json/index.ts
+++ src/json/index.ts
@@ -72,14 +72,30 @@ export const object = {
   required: true,
 } as const satisfies Validator<UnvalidatedObject<unknown>>;
 
-/** Transforms a validator to be optional. */
-export function optional<T>(validator: Validator<T>) {
+/**
+ * Transforms a validator to be optional, accepting `undefined` or `null` for an
+ * absent value.
+ */
+export function optionalOrNull<T>(validator: Validator<T>) {
   return {
     validate: (val: unknown) => {
       return val === undefined || val === null || validator.validate(val);
     },
     required: false,
   } as const satisfies Validator<T | undefined | null>;
+}
+
+/**
+ * Transforms a validator to be optional, accepting `undefined` for an absent
+ * value but, unlike `optionalOrNull`, rejecting `null`.
+ */
+export function optional<T>(validator: Validator<T>) {
+  return {
+    validate: (val: unknown): val is T | undefined => {
+      return val === undefined || validator.validate(val);
+    },
+    required: false,
+  } as const satisfies Validator<T | undefined>;
 }
 
 /** Represents an arbitrary object schema. */
